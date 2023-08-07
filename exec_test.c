f0a2ae8eb36eaed517aa431cb5ef4e42be9f0b8d
Mohamed Berroukham <mberrouk@e2r2p7.1337.ma>
2023-08-07 01:53:46 +0100

    handl_exec_test.c

--- exec_test.c
+++ exec_test.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec_test.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hoakoumi <hoakoumi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mberrouk <mberrouk@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 19:42:34 by mberrouk          #+#    #+#             */
-/*   Updated: 2023/08/07 00:36:44 by hoakoumi         ###   ########.fr       */
+/*   Updated: 2023/08/07 01:52:25 by mberrouk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 #include <sys/fcntl.h>
 #include <unistd.h>
 #include "include/shell.h"
+void	builtins_main(t_env **env_list, t_cmd *cmds);
+int	builtin_status(char **str);
 
 void	not_found(char *parm)
 {
@@ -92,12 +94,12 @@ char	**find_path(char **env)
 	return (NULL);
 }
 
-void	execute_command(int ifd, int ofd, t_cmd *data, char **cmds, char **path, char **env, int ff)
+void	execute_command(int ifd, int *pip, t_cmd *data, char **cmds, char **path, char **env)
 {
 	pid_t	pid;
 	char	*cmd;
 	(void)data;
-	//(void)ff;
+
 	pid = fork();
 	if (pid == 0)
 	{
@@ -106,39 +108,83 @@ void	execute_command(int ifd, int ofd, t_cmd *data, char **cmds, char **path, ch
 				puterr("1 :Error dup2\n");
 		if (ifd == -1)
 			return ;
-		if (ofd > 1)
-			if (dup2(ofd, STDOUT_FILENO) == -1)
-				puterr("2 :Error dup2\n");
-		if (ofd == -1)
+		if (pip[1] > 1)
 		{
-			close(ifd);
-			return ;
+			if (dup2(pip[1], STDOUT_FILENO) == -1)
+				puterr("2 :Error dup2\n");
+			if (pip[1] == -1)      							 /** for the failure of dup2 **/
+			{
+				close(ifd);
+				return ;
+			}
 		}
-		if (ofd > 1)
-			close(ofd);
+		if (pip[1] > 1) 										/** close all fd files**/
+			close(pip[1]);
 		if (ifd > 0)
 			close(ifd);
-	 	if (ff > 0)
-			close(ff);
-		if (cmds && cmds[0] && (cmds[0][0] == '/' || cmds[0][0] == '.'))
-			cmd = cmds[0];
-		else
-			cmd = ft_access(path, cmds[0]);
-		if (!cmd)
+	 	if (pip[0] > 0)
+			close(pip[0]);
+		if (builtin_status(data->cmd) != -1)					/** check for builtin **/
 		{
-			not_found(cmds[0]);
-			exit(127);
-		}
-		if (builtin_status(data->cmd) != -1)
 			builtins_main(&g_info.g_env, data);
-		else if (execve(cmd, cmds, env) == -1)
+			exit(0);
+		}
+		else
 		{
-			puterr(NULL);
-			exit(errno);
+			if (cmds && cmds[0] && (cmds[0][0] == '/' || cmds[0][0] == '.'))
+				cmd = cmds[0];
+			else
+				cmd = ft_access(path, cmds[0]);
+			if (!cmd)
+			{
+				not_found(cmds[0]);
+				exit(127);
+			}
+			if (execve(cmd, cmds, env) == -1)
+			{
+				puterr(NULL);
+				exit(errno);
+			}
 		}
 	}
 	else if (pid < 0)
 		puterr(NULL);
+}
+
+void	open_fd_file(t_cmd *data, int *fdi, int *fdo, int *fdapp)
+{
+	while (data->file)
+	{
+		if (data->file->type == INPUT_RE)
+		{
+			if (*fdi != 0)
+				close(*fdi);
+			*fdi = open(data->file->name, O_RDONLY);
+			if (*fdi == -1)
+				perror(data->file->name);
+		}
+		else if (data->file->type == OUTPUT_RE)
+		{
+			data->type = data->file->type;
+			if (*fdo != 1)
+				close(*fdo);
+			*fdo = open(data->file->name, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+			if (*fdo == -1)
+				perror(data->file->name);
+		}
+		else if (data->file->type == APPEND_RE)
+		{
+			data->type = data->file->type;
+			if (*fdapp != -1)
+				close(*fdapp);
+			*fdapp = open(data->file->name, O_APPEND | O_CREAT | O_RDWR);
+			if (*fdapp == -1)
+				perror(data->file->name);
+		}
+		//else if (data->file->type == HERE_DOC)
+		//	*fdi = data->file->fd;
+		data->file = data->file->next;
+	}
 }
 
 void	cmds(t_cmd *data, int fdi, char **path, char **env)
@@ -146,7 +192,7 @@ void	cmds(t_cmd *data, int fdi, char **path, char **env)
 	int	pip_fds[2];
 	int	fdo;
 	int	fdapp = -1;
-	
+
 	pip_fds[1] = 1; 
 	pip_fds[0] = 0; 
 	while (data)
@@ -154,59 +200,28 @@ void	cmds(t_cmd *data, int fdi, char **path, char **env)
 		fdo = 1;
 		if (!data->cmd && !data->file)
 			return ;
-		while (data->file)
+		open_fd_file(data, &fdi, &fdo, &fdapp);
+		if (data->type == APPEND_RE)
 		{
-			if (data->file->type == INPUT_RE)
-			{
-				if (fdi != 0)
-					close(fdi);
-				fdi = open(data->file->name, O_RDONLY);
-				if (fdi == -1)
-					perror(data->file->name);
-			}
-			else if (data->file->type == OUTPUT_RE)
-			{
-				data->type = data->file->type;
-				if (fdo != 1)
-					close(fdo);
-				fdo = open(data->file->name, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-				if (fdo == -1)
-					perror(data->file->name);
-				//printf("%d %s\n", fdo, data->file->name);
-			}
-			else if (data->file->type == APPEND_RE)
-			{
-				data->type = data->file->type;
-				if (fdapp != -1)
-					close(fdapp);
-				fdapp = open(data->file->name, O_APPEND | O_CREAT | O_RDWR);
-				if (fdapp == -1)
-					perror(data->file->name);
-				
-			}
-			data->file = data->file->next;
+			if (fdo != 1)
+				close(fdo);
+			fdo = fdapp;
 		}
-
-	
 		if (data->next)
 		{	
-			if (pip_fds[1] != 1)
-				close(pip_fds[1]);
 			if (pipe(pip_fds) == -1)
 				puterr(NULL);
-		}
-			if (data->type == APPEND_RE)
+			if (fdo > 1)
 			{
-				if (fdo != 1)
-					close(fdo);
-				fdo = fdapp;
+				close(pip_fds[1]);
+				pip_fds[1] = fdo;
 			}
-			if (pip_fds[1] != 1)
-			close(pip_fds[1]);
+		}
+		else
+		{
 			pip_fds[1] = fdo;
-		
-		
-		execute_command(fdi, pip_fds[1], data, data->cmd, path, env, pip_fds[0]);
+		}
+		execute_command(fdi, pip_fds, data, data->cmd, path, env);
 		if (fdi != 0)
 			close(fdi);
 		if (pip_fds[1] != 1)
@@ -216,7 +231,6 @@ void	cmds(t_cmd *data, int fdi, char **path, char **env)
 	}
 	if (fdi != 0)
 		close(fdi);
-
 }
 
 int	ft_lstsize_s(char **lst)
@@ -235,40 +249,30 @@ int	ft_lstsize_s(char **lst)
 
 int	builtin_status(char **str)
 {
-	if (ft_strncmp("cd", str[0], 3) == 0)
+	if (!str)
+		return (-1);
+	if (!ft_strncmp("cd", str[0], 3))
+		return (1);
+	if (!ft_strncmp("pwd", str[0], 4))
 		return (0);
-	if (ft_strncmp("pwd", str[0], 4) == 0)
+	if (!ft_strncmp("echo", str[0], 5))
 		return (0);
-	if (ft_strncmp("echo", str[0], 5) == 0)
+	if (!ft_strncmp("exit", str[0], 5))
+		return (1);
+	if (!ft_strncmp("env", str[0], 4))
 		return (0);
-	if (ft_strncmp("exit", str[0], 5) == 0)
-		return (0);
-	if (ft_strncmp("env", str[0], 4) == 0)
-		return (0);
-	if (ft_strncmp("unset", str[0], 6) == 0)
-		return (0);
-	if (ft_strncmp("export", str[0], 7) == 0)
-		return (0);
-		// {
-		// 	if (!str[1])
-		// 		return (0);
-		// 	return (1);
-		// }
+	if (!ft_strncmp("unset", str[0], 6))
+		return (1);
+	if (!ft_strncmp("export", str[0], 7))
+	{
+		if (!str[1])
+			return (0);
+		return (1);
+	}
 	return (-1);
 }
 
-int size_cmds(t_cmd *cmds)
-{
-    int i;
-    
-    i = 0;
-	while(cmds)
-	{
-		i++;
-		cmds = cmds->next;
-	}
-    return (i);
-}
+int size_cmds(t_cmd *cmds);
 
 void	exec_cmds(t_cmd *data, int status, char **env)
 {
@@ -282,15 +286,11 @@ void	exec_cmds(t_cmd *data, int status, char **env)
 		builtins_main(&g_info.g_env, data);
 	else
 	{
-	path = find_path(env);
-	if (!path)
-	{
-		printf("\n Error : path\n");
-	}
-	cmds(data, 0, path, env);
-	while (wait(&status) > 0)
-		;
-	status = WEXITSTATUS(status);
-	free_double(path);
+		path = find_path(env);
+		cmds(data, 0, path, env);
+		while (wait(&status) > 0)
+			;
+		status = WEXITSTATUS(status);
+		free_double(path);
 	}
 }
