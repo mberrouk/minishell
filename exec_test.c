e273c4d510d5083720b56ccc3134ffe962ab35d7
Houda Akoumi <hoakoumi@e1r11p12.1337.ma>
2023-08-07 10:49:52 +0100

    exec

--- exec_test.c
+++ exec_test.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec_test.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mberrouk <mberrouk@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: hoakoumi <hoakoumi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 19:42:34 by mberrouk          #+#    #+#             */
-/*   Updated: 2023/08/07 04:24:25 by mberrouk         ###   ########.fr       */
+/*   Updated: 2023/08/07 10:49:21 by hoakoumi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,12 +32,14 @@ void	puterr(char *str)
 	if (str)
 	{
 		write(STDERR_FILENO, str, ft_strlen(str));
-		exit(errno);
+		//exit(errno);
+		exit(g_info.exit_status);
 	}
 	else
 	{
 		perror("Error");
-		exit(errno);
+		//exit(errno);
+		exit(g_info.exit_status);
 	}
 }
 
@@ -151,86 +153,99 @@ void	execute_command(int ifd, int *pip, t_cmd *data, char **cmds, char **path, c
 		puterr(NULL);
 }
 
-void	open_fd_file(t_cmd *data, int *fdi, int *fdo, int *fdapp)
+void	open_fd_file(t_cmd *data, int *fd_inp, int *fd_oup, int *fd_app)
 {
 	while (data->file)
 	{
 		if (data->file->type == INPUT_RE)
 		{
-			if (*fdi != 0)
-				close(*fdi);
-			*fdi = open(data->file->name, O_RDONLY);
-			if (*fdi == -1)
+			if (*fd_inp != 0)
+				close(*fd_inp);
+			*fd_inp = open(data->file->name, O_RDONLY);
+			if (*fd_inp == -1)
+			{
+				_print(2, "minishell: ");
 				perror(data->file->name);
+				g_info.exit_status = 1;
+
+			}
 		}
 		else if (data->file->type == OUTPUT_RE)
 		{
 			data->type = data->file->type;
-			if (*fdo != 1)
-				close(*fdo);
-			*fdo = open(data->file->name, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-			if (*fdo == -1)
+			if (*fd_oup != 1)
+				close(*fd_oup);
+			*fd_oup = open(data->file->name, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+			if (*fd_oup == -1)
+			{
+				_print(2, "minishell: ");
 				perror(data->file->name);
+				g_info.exit_status = 1;
+
+			}
 		}
 		else if (data->file->type == APPEND_RE)
 		{
 			data->type = data->file->type;
-			if (*fdapp != -1)
-				close(*fdapp);
-			*fdapp = open(data->file->name, O_APPEND | O_CREAT | O_RDWR);
-			if (*fdapp == -1)
+			if (*fd_app != -1)
+				close(*fd_app);
+			*fd_app = open(data->file->name, O_APPEND | O_CREAT | O_RDWR);
+			if (*fd_app == -1)
+			{
+				_print(2, "minishell: ");
 				perror(data->file->name);
+				g_info.exit_status = 1;
+
+			}
 		}
 		//else if (data->file->type == HERE_DOC)
-		//	*fdi = data->file->fd;
+		//	*fd_inp = data->file->fd;
 		data->file = data->file->next;
 	}
 }
 
-void	cmds(t_cmd *data, int fdi, char **path, char **env)
+void	cmds(t_cmd *data, int fd_inp, char **path, char **env)
 {
 	int	pip_fds[2];
-	int	fdo;
-	int	fdapp = -1;
+	int	fd_oup;
+	int	fd_app = -1;
 
 	pip_fds[1] = 1; 
 	pip_fds[0] = 0; 
-	while (data)
+	while (data != NULL)
 	{
-		fdo = 1;
+		fd_oup = 1;
 		if (!data->cmd && !data->file)
 			return ;
-		open_fd_file(data, &fdi, &fdo, &fdapp);
+		open_fd_file(data, &fd_inp, &fd_oup, &fd_app);
 		if (data->type == APPEND_RE)
 		{
-			if (fdo != 1)
-				close(fdo);
-			fdo = fdapp;
+			if (fd_oup != 1)
+				close(fd_oup);
+			fd_oup = fd_app;
 		}
 		if (data->next)
 		{	
 			if (pipe(pip_fds) == -1)
 				puterr(NULL);
-			if (fdo > 1)
+			if (fd_oup > 1)
 			{
 				close(pip_fds[1]);
-				pip_fds[1] = fdo;
+				pip_fds[1] = fd_oup;
 			}
 		}
 		else
-		{
-			pip_fds[1] = fdo;
-		}
-		execute_command(fdi, pip_fds, data, data->cmd, path, env);
-		if (fdi != 0)
-			close(fdi);
+			pip_fds[1] = fd_oup;
+		execute_command(fd_inp, pip_fds, data, data->cmd, path, env);
+		if (fd_inp != 0)
+			close(fd_inp);
 		if (pip_fds[1] != 1)
 			close(pip_fds[1]);
-		fdi = pip_fds[0];
+		fd_inp = pip_fds[0];
 		data = data->next;
 	}
-	if (fdi != 0)
-		close(fdi);
+	if (fd_inp != 0)
+		close(fd_inp);
 }
 
 int	ft_lstsize_s(char **lst)
